ec40f846d5112aae360e3c38a3ad2e6aa15290d2
candleeee <herlina.carolina1@gmail.com>
2024-04-04 23:39:52 +0700

    first commit

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -8,55 +8,90 @@
     @vite('resources/sass/app.scss')
 </head>
 <body>
-
-    <div class="container-sm mt-5">
-        <div class="row justify-content-center">
-            <div class="col-xl-6">
-                <div class="p-5 bg-light rounded-3 border">
-                    <h1 class="mb-4 text-center">{{ $pageTitle }}</h1>
-                    <hr class="mb-4">
-
+    @extends('layouts.app')
+    @section('content')
+    <div class="container-sm my-5">
+        <form action="{{ route('employees.update', ['employee' => $employee->id]) }}" method="POST">
+            @csrf
+            @method('put')
+            <div class="row justify-content-center">
+                <div class="container-sm mt-5">
                     <form action="{{ route('employees.update', ['employee' => $employee->id]) }}" method="POST">
                         @csrf
-                        @method('PUT')
-
-                        <div class="mb-3">
-                            <label for="firstName" class="form-label">First Name</label>
-                            <input type="text" id="firstName" name="firstName" class="form-control" value="{{ $employee->firstname }}">
+                        @method('put')
+                        <div class="row justify-content-center">
+                            <div class="p-5 bg-light rounded-3 col-xl-6">
+                                <div class="mb-3 text-center">
+                                    <i class="bi-person-circle fs-1"></i>
+                                    <h4>Edit Employee</h4>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="firstName" class="formlabel">First Name</label>
+                                        <input class="form-control @error('firstName') is-invalid @enderror" type="text" name="firstName" id="firstName" value="{{ $errors->any() ? old('firstName') : $employee->firstname }}" placeholder="Enter First Name">
+                                        @error('firstName')
+                                            <div class="textdanger"><small>{{ $message }}</small></div>
+                                        @enderror
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="lastName" class="form-label">LastName</label>
+                                        <input class="form-control @error('lastName') is-invalid @enderror" type="text" name="lastName" id="lastName" value="{{ $errors->any() ? old('lastName') : $employee->lastname }}" placeholder="Enter Last Name">
+                                        @error('lastName')
+                                            <div class="textdanger"><small>{{ $message }}</small></div>
+                                        @enderror
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="email" class="formlabel">Email</label>
+                                        <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" id="email" value="{{ $errors->any() ? old('email') : $employee->email }}" placeholder="Enter Email">
+                                        @error('email')
+                                            <div class="textdanger"><small>{{ $message }}</small></div>
+                                        @enderror
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="age" class="formlabel">Age</label>
+                                        <input class="form-control @error('age') is-invalid @enderror" type="text" name="age" id="age" value="{{ $errors->any() ? old('age') : $employee->age }}" placeholder="Enter Age">
+                                        @error('age')
+                                            <div class="textdanger"><small>{{ $message }}</small></div>
+                                        @enderror
+                                    </div>
+                                    <div class="col-md-12 mb-3">
+                                        <label for="position" class="formlabel">Position</label>
+                                        <select name="position" id="position" class="form-select">
+                                            @php
+                                                $selected = "";
+                                                if ($errors->any())
+                                                    $selected = old('position');
+                                                else
+                                                    $selected = $employee->position_id;
+                                            @endphp
+                                            @foreach ($positions as $position)
+                                                <option value="{{ $position->id }}" {{ $selected == $position->id ? 'selected' : '' }}>{{ $position->code.'-'.$position->name }}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('position')
+                                            <div class="textdanger"><small>{{ $message }}</small></div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-md-6 d-grid">
+                                        <a href="{{ route('employees.index') }}" class="btn btn-outline-dark btn-lg mt-3"><i class="bi-arrow-left-circle me-2"></i> Cancel</a>
+                                    </div>
+                                    <div class="col-md-6 d-grid">
+                                        <button type="submit" class="btn btn-dark btn-lg mt-3"><i class="bi-check-circle me-2"></i> Edit</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-
-                        <div class="mb-3">
-                            <label for="lastName" class="form-label">Last Name</label>
-                            <input type="text" id="lastName" name="lastName" class="form-control" value="{{ $employee->lastname }}">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" id="email" name="email" class="form-control" value="{{ $employee->email }}">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="age" class="form-label">Age</label>
-                            <input type="number" id="age" name="age" class="form-control" value="{{ $employee->age }}">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="position" class="form-label">Position</label>
-                            <select id="position" name="position" class="form-select">
-                                @foreach($positions as $position)
-                                    <option value="{{ $position->id }}" @if($position->id == $employee->position_id) selected @endif>{{ $position->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        <button type="submit" class="btn btn-dark">Update Employee</button>
                     </form>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
+@endsection
 
-    @vite('resources/js/app.js')
+@vite('resources/js/app.js')
 </body>
 </html>
-
